484ce3c93b42e4fdfdbdafd4cc80efc0be6d53a6
ArtEze <passymas@gmail.com>
2022-01-08 17:49:44 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,7 @@ $bibma = crear_bibliomatriz("b","B",0);
 echo "<div>Registro:</div>";
 echo mostrar_log($bibma->registro);
 
+$eliminado = borrar_bibliomatriz($bibma);
+echo "<div>Borrando bibliomatriz:</div>";
+echo mostrar_log($eliminado->registro);
 ?>
