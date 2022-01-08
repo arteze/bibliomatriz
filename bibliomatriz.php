406a2d0dcbbe87b3415671a298f5143b63910ccd
ArtEze <passymas@gmail.com>
2022-01-08 17:15:14 +0000

    Update bibliomatriz.php

--- bibliomatriz.php
+++ bibliomatriz.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
+ini_set("display_errors","1");
+ini_set("display_startup_errors","1");
 error_reporting(E_ALL);
 
 function mostrar_log($v){
@@ -9,33 +9,33 @@ function mostrar_log($v){
 	echo "<pre style='background-color: #eee'>".htmlentities($volcado)."</pre>";
 }
 function borrar_archivo($ruta,$o){
-	$bin_url_inicio_barra = substr($ruta,0,1)== "/";
+	$bin_url_inicio_barra = substr($ruta,0,1)=="/";
 	if($bin_url_inicio_barra){
-		array_push($o->registro,"ebia1_$o->unibinop Advertencia: El archivo '$ruta' es del sistema, por eso no se va a borrar.");
+		array_push($o->registro,"ebia1 Advertencia: El archivo '$ruta' es del sistema, por eso no se va a borrar.");
 	}else{
 		$bin_existe_archivo = file_exists($ruta);
 		if($bin_existe_archivo){
 			$bin_borrar_archivo = unlink($ruta);
 			if($bin_borrar_archivo){
-				array_push($o->registro,"ebia3_$o->unibinop Archivo '$ruta' borrado correctamente.");
+				array_push($o->registro,"ebia3 Archivo '$ruta' borrado correctamente.");
 			}else{
-				array_push($o->registro,"ebia2_$o->unibinop El archivo o carpeta '$ruta' existe, pero no se pudo borrar.");
+				array_push($o->registro,"ebia2 El archivo o carpeta '$ruta' existe, pero no se pudo borrar.");
 			}
 		}else{
-			array_push($o->registro,"ebia4_$o->unibinop Advertencia: La ruta '$ruta' no contiene información, por eso no se va a eliminar su contenido.");
+			array_push($o->registro,"ebia4 Advertencia: La ruta '$ruta' no contiene información, por eso no se va a borrar su contenido.");
 		}
 	}
 }
 function borrar_carpeta($ruta,$o){
 	$bin_url_inicio_barra = substr($ruta,0,1)== "/";
 	if($bin_url_inicio_barra){
-		array_push($o->registro,"ebia1_$o->unibinop Advertencia: La carpeta '$ruta' es del sistema, por eso no se va a borrar.");
+		array_push($o->registro,"ebia1 Advertencia: La carpeta '$ruta' es del sistema, por eso no se va a borrar.");
 	}else{
 		$bin_borrar_carpeta = rmdir($ruta);
 		if($bin_borrar_carpeta){
-			array_push($o->registro,"ebca1_$o->unibinop Carpeta '$ruta' borrada correctamente.");
+			array_push($o->registro,"ebca1 Carpeta '$ruta' borrada correctamente.");
 		}else{
-			array_push($o->registro,"ebca0_$o->unibinop Advertencia: La carpeta '$ruta' no se pudo borrar.");
+			array_push($o->registro,"ebca0 Advertencia: La carpeta '$ruta' no se pudo borrar.");
 		}
 	}
 }
@@ -43,7 +43,7 @@ function borrar_url($ruta,$o){
 	if($ruta){
 		$rutas = glob( $ruta ."/*", GLOB_MARK );
 		foreach( $rutas as $url ){
-			if(substr($url,-1)== "/"){
+			if(substr($url,-1)=="/"){
 				borrar_url($url,$o);
 			}else{
 				borrar_archivo($url,$o);
@@ -56,32 +56,32 @@ function borrar_url($ruta,$o){
 			borrar_archivo($ruta,$o);
 		}
 	}else{
-		array_push($o->registro,"ebu_$o->unibinop Advertencia: La ruta '$ruta' es nula, por eso no se va a intentar borrar ningún archivo");
+		array_push($o->registro,"ebu Advertencia: La ruta '$ruta' es nula, por eso no se va a intentar borrar ningún archivo");
 	}
 	return $o;
 }
 function crear_carpeta($ruta,$o){
 	$bin_crear_carpeta = mkdir($ruta, 0777, true);
 	if($bin_crear_carpeta){
-		array_push($o->registro,"ecap1_$o->unibinop Carpeta '$ruta' creada correctamente.");
+		array_push($o->registro,"ecap1 Carpeta '$ruta' creada correctamente.");
 	}else{
-		array_push($o->registro,"ecap0_$o->unibinop Advertencia: La carpeta '$ruta' no se pudo crear.");
+		array_push($o->registro,"ecap0 Advertencia: La carpeta '$ruta' no se pudo crear.");
 	}
 }
 function crear_archivo($ruta,$datos,$o){
 	$var_crear_archivo = file_put_contents($ruta,$datos);
 	if($var_crear_archivo===strlen($datos)){
-		array_push($o->registro,"ecai1_$o->unibinop Archivo '$ruta' creado correctamente.");
+		array_push($o->registro,"ecai1 Archivo '$ruta' creado correctamente.");
 	}else{
 		if($var_crear_archivo===false){
-			array_push($o->registro,"ecai3_$o->unibinop Advertencia: El archivo '$ruta' no se pudo crear.");
+			array_push($o->registro,"ecai3 Advertencia: El archivo '$ruta' no se pudo crear.");
 		}else{
-			array_push($o->registro,"ecai2_$o->unibinop Advertencia: El archivo '$ruta' se pudo crear, pero su contenido es parcial.");
+			array_push($o->registro,"ecai2 Advertencia: El archivo '$ruta' se pudo crear, pero su contenido es parcial.");
 		}
 	}
 }
 function registro_url_tiene_info($ruta,$o){
-	array_push($o->registro,"euti_$o->unibinop La ruta '$ruta' contiene información.");
+	array_push($o->registro,"euti La ruta '$ruta' contiene información.");
 }
 function crear_carpeta_si_no_existe($ruta,$o){
 	$retorno = 0;
@@ -89,7 +89,7 @@ function crear_carpeta_si_no_existe($ruta,$o){
 	if($bin_existe_url){
 		$bin_es_carpeta = is_dir("$ruta");
 		if($bin_es_carpeta){
-			array_push($o->registro,"euti_$o->unibinop La carpeta '$ruta' existía.");
+			array_push($o->registro,"euti La carpeta '$ruta' existía.");
 			$retorno = 3;
 		}else{
 			borrar_url($ruta,$o);
@@ -112,7 +112,7 @@ function crear_archivo_si_no_existe($ruta,$datos,$o){
 			crear_archivo($ruta,$datos,$o);
 			$retorno = 3;
 		}else{
-			array_push($o->registro,"euti_$o->unibinop El archivo '$ruta' existía.");
+			array_push($o->registro,"euti El archivo '$ruta' existía.");
 			$retorno = 2;
 		}
 	}else{
@@ -163,12 +163,14 @@ function crear_bibliomatriz($ruta,$nombre,$unibinop){
 		}
 		if($unificado==1){
 			$real_url_creada = crear_archivo_si_no_existe($o->url,$o->datos,$o);
-			if( $real_url_creada==1 || $real_url_creada==3 ){
-				// Resto
-			}
 		}
 	}
 	return $o;
 }
-
+function borrar_bibliomatriz($bibma){
+	$o = (object)array();
+	$o->registro = array();
+	borrar_url($bibma->url,$o);
+	return $o;
+}
 ?>
