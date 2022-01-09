154cfc65e373b17cdeadf9615d0365a571e055f7
ArtEze <passymas@gmail.com>
2022-01-09 01:46:26 +0000

    Update bibliomatriz.js

--- bibliomatriz.js
+++ bibliomatriz.js
@@ -1,11 +1,32 @@
-function mostrar_log(registros_nuevos){
+function mostrar_log(registros){
 	var pre = document.querySelector("pre")
-	var registros = [...pre.querySelectorAll("a")].map(x=>x.innerHTML)
-	var i = 0
-	registros.push(...registros_nuevos)
-	pre.innerHTML = registros.filter(x=>x!=0).map(x=>{
-		return (++i) + " <a>" + x + "</a>"
-	}).join("\n")
+	var div = document.createElement("div")
+	var ndivs = [...pre.querySelectorAll("div")].length
+	var ite = [...pre.querySelectorAll(".n1")].slice(-1)[0]
+	var i = ite && +ite.innerHTML || 0
+	var j = 0
+	if(ndivs%2){
+		div.style["background-color"] = "#eeee"
+	}else{
+		div.style["background-color"] = "#ddde"
+	}
+	if(/^(\s|\t)+$/.test(pre.innerHTML)){
+		pre.innerHTML = ""
+	}
+	registros.map(x=>{
+		var a = document.createElement("a")
+		var b = document.createElement("a")
+		var c = document.createElement("a")
+		a.className = "n1"
+		b.className = "n2"
+		a.innerHTML = `${++i}`
+		b.innerHTML = ` ${++j} `
+		c.innerHTML = x + "\n"
+		div.appendChild(a)
+		div.appendChild(b)
+		div.appendChild(c)
+	})
+	pre.appendChild(div)
 	return pre
 }
 function vaciar_log(){
@@ -14,12 +35,12 @@ function vaciar_log(){
 }
 
 function crear_bibliomatriz(){
-	var tipo = document.querySelector("#tipo").checked
-	var unificado = document.querySelector("#unificado").checked
+	var tipo = +document.querySelector("#tipo").checked
+	var unificado = +document.querySelector("#unificado").checked
 	var ruta = document.querySelector("#ruta").value
 	var nombre = document.querySelector("#nombre").value
-	var unibinop = tipo * 2 + unificado
-	var f = (fetch(`bibliomatriz.php?f=c&r=${ruta}&n=${nombre}&u=${unibinop}`)
+	if(unificado){ruta += "."}
+	var f = (fetch(`bibliomatriz.php?f=c&r=${ruta}&n=${nombre}&t=${tipo}`)
 		.then(x=>x.text())
 		.then(x=>{
 			var analizado = JSON.parse(x)
@@ -29,10 +50,10 @@ function crear_bibliomatriz(){
 	)
 }
 function borrar_bibliomatriz(){
-	var unificado = document.querySelector("#unificado").checked
+	var unificado = +document.querySelector("#unificado").checked
 	var ruta = document.querySelector("#ruta").value
-	var unibinop = tipo * 2 + unificado
-	var f = (fetch(`bibliomatriz.php?f=b&r=${ruta}&u=${unibinop}`)
+	if(unificado){ruta += "."}
+	var f = (fetch(`bibliomatriz.php?f=b&r=${ruta}`)
 		.then(x=>x.text())
 		.then(x=>{
 			var analizado = JSON.parse(x)
